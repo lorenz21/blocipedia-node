1d764a7411bcea8b385fde4989f4f4c8b26d1a72
Steve <lorenz21@hawaii.edu>
2018-10-18 16:13:38 -1000

    Checkpoint 2 - User Sign up: completed

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,14 @@
 module.exports = {
    init(app){
       const staticRoutes = require("../routes/static");
+      const userRoutes = require("../routes/users");
+
+      if(process.env.NODE_ENV === "test"){
+         const mockAuth = require("../../spec/support/mock-auth");
+         mockAuth.fakeIt(app);
+      }
+
       app.use(staticRoutes);
+      app.use(userRoutes);
    }
 }
