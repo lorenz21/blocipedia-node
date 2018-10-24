312473fa3a5eac839703b5bc3a722b9bcc4040b1
Steve <lorenz21@hawaii.edu>
2018-10-23 15:13:22 -1000

    Checkpoint 7 - upgrading account:completed

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,10 +8,18 @@ router.get("/users/signup", userController.signUp);
 
 router.post("/users/signup", validation.validateCreateUsers, userController.create);
 
+router.get("/users/payment", userController.payment);
+
 router.get("/users/signin", userController.signInForm);
 
 router.post("/users/signin", validation.validateSignUsers, userController.signIn);
 
 router.get("/users/signout", userController.signOut);
+
+router.get("/users/:id", userController.show);
+
+router.post("/users/:id/updatePremium", userController.updatePremium);
+
+router.post("/users/:id/updateStandard", userController.updateStandard);
 
 module.exports = router;
